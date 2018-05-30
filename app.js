05de313c6c1fe6b1853312c9bc4634cff4b591f8
Ryan Strauss <ryanrstrauss@icloud.com>
2018-05-30 09:11:12 -0400

    Reformatting

--- app.js
+++ app.js
@@ -17,57 +17,57 @@ const dbName = 'todoApp';
 //   const db = client.db(dbName);
 //   // Get the documents collection
 //   const collection = db.collection('todos');
-  // Insert some documents
-  /*for(let i=0; i<20000; i++){
-      var randomDescription = faker.lorem.words();
-        var randomCompletedOn = faker.date.future();
-        var randomCreatedOn = faker.date.past();
-        collection.insert({description: randomDescription, completedOn: randomCompletedOn, createdOn: randomCreatedOn});
-  }*/
+// Insert some documents
+/*for(let i=0; i<20000; i++){
+    var randomDescription = faker.lorem.words();
+      var randomCompletedOn = faker.date.future();
+      var randomCreatedOn = faker.date.past();
+      collection.insert({description: randomDescription, completedOn: randomCompletedOn, createdOn: randomCreatedOn});
+}*/
 
-  mongoose.connect(url);
-  var db = mongoose.connection;
+mongoose.connect(url);
+var db = mongoose.connection;
 
-  var TodoSchema = new mongoose.Schema(
+var TodoSchema = new mongoose.Schema(
     {
-      description: {
-        type: String,
-        maxlength: 100
-      },
-      completedOn: {
-        type: Date
-      },
-      createdOn: {
-        type: Date
-      }
+        description: {
+            type: String,
+            maxlength: 100
+        },
+        completedOn: {
+            type: Date
+        },
+        createdOn: {
+            type: Date
+        }
     },
     {collection: 'todos'}
-  );
-  var UserSchema = new mongoose.Schema(
+);
+var UserSchema = new mongoose.Schema(
     {
-      firstName: {
-        type: String,
-        maxlength: 100
-      },
-      lastName: {
-        type: String,
-        maxlength: 100
-      },
-      userName: {
-        type: String,
-        maxlength: 100
-      },
-      email: {
-        type: String,
-        maxlength: 100
-      }
+        firstName: {
+            type: String,
+            maxlength: 100
+        },
+        lastName: {
+            type: String,
+            maxlength: 100
+        },
+        userName: {
+            type: String,
+            maxlength: 100
+        },
+        email: {
+            type: String,
+            maxlength: 100
+        }
     },
     {collection: 'users'}
-  );
-  var Todos = mongoose.model('Todos', TodoSchema);
-  var Users = mongoose.model('Users', UserSchema);
+);
+var Todos = mongoose.model('Todos', TodoSchema);
+var Users = mongoose.model('Users', UserSchema);
 
-  db.once('open', function(){
+db.once('open', function () {
     /*for(let i=0; i<20000; i++){
         var randomDescription = faker.lorem.words();
           var randomCompletedOn = faker.date.future();
@@ -75,15 +75,21 @@ const dbName = 'todoApp';
           const newTodo = new Todos({description:randomDescription, completedOn: randomCompletedOn, createdOn: randomCreatedOn});
           newTodo.save().then(() => console.log("WORKED!\n"));
     }*/
-    for(let i=0; i<2; i++){
-        var randomFirstName = faker.name.firstName();;
-          var randomLastName = faker.name.lastName();
-          var randomUserName = faker.name.findName();
-          var randomEmail = faker.internet.email();
-          const newUser = new Users({firstName:randomFirstName, lastName: randomLastName, userName: randomUserName, email: randomEmail});
-          newUser.save().then(() => console.log("WORKED!\n"));
+    for (let i = 0; i < 2; i++) {
+        var randomFirstName = faker.name.firstName();
+        ;
+        var randomLastName = faker.name.lastName();
+        var randomUserName = faker.name.findName();
+        var randomEmail = faker.internet.email();
+        const newUser = new Users({
+            firstName: randomFirstName,
+            lastName: randomLastName,
+            userName: randomUserName,
+            email: randomEmail
+        });
+        newUser.save().then(() => console.log("WORKED!\n"));
     }
-  });
+});
 
-  // client.close();
+// client.close();
 // });
